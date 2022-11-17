67559103a35000d3f9d2cce5028fe42b8b26bb31
mdsohag65 <sohag92ahmed@gmail.com>
2022-11-17 12:29:28 +0600

    load places in ui

---
+++ src/Pages/Home/Place/Place.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Place = ({ place }) => {
+    const { name, img, description, price } = place;
+    return (
+        <div class="card w-80 bg-base-100 shadow-xl">
+            <figure><img className='image' src={img} alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title text-secondary">{name}</h2>
+                <p>{description}</p>
+                <p className='text-accent'>Price: ${price}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Review</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Place;
--- src/Pages/Home/Places/Places.js
+++ src/Pages/Home/Places/Places.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Place from '../Place/Place';
 
 const Places = () => {
+    const [places, setPlaces] = useState([]);
+    useEffect(() => {
+        fetch('Places.json')
+            .then(res => res.json())
+            .then(data => setPlaces(data))
+    }, [])
     return (
-        <div>
-            <h2>places</h2>
+        <div className='my-9'>
+            <h2 className='text-3xl text-center font-bold mb-3'>Visit <span className='text-secondary'>Places</span></h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ml-10'>
+                {
+                    places.map(place => <Place
+                        key={place._id}
+                        place={place}
+                    ></Place>)
+                }
+            </div>
         </div>
     );
 };
